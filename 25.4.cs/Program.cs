1a6b78acecf17c58fb17bd844077b51f0ecc42b5
gutblender <43554226+gutblender@users.noreply.github.com>
2025-12-03 23:57:58 -0600

    un-readability
    delete a bunch of braces and whitespace and just make it evilly unreadable and l33t!5t as sh!t

--- 25.4.cs/Program.cs
+++ 25.4.cs/Program.cs
@@ -29,35 +29,22 @@ internal class Program
         {
             int total = 0;
             for (int y = 0; y < map.Count; y++)
-            {
-                for (int x = 0; x < map[y].Length; x++)
-                {
+                for (int x = 0, cnt = 0; x < map[y].Length; x++, cnt = 0)
+                { // at this position, look all around.
                     if (map[y][x] != Roll)
-                        continue; // not a roll, fuck you!
-
-                    // at this position, look all around.
-                    int cnt = 0; // are there less than TooManyRolls rolls around me?
-                    bool ok = true;
-
-                    for (int j = Math.Max(0, y - 1); ok && j <= Math.Min(y + 1, map.Count - 1); j++) // y lookaround
-                    {
-                        for (int i = Math.Max(0, x - 1); ok && i <= Math.Min(x + 1, map[j].Length - 1); i++) // x lookaround
-                        {
+                        continue;
+                    for (int j = Math.Max(0, y - 1); cnt < TooManyRolls && j <= Math.Min(y + 1, map.Count - 1); j++) // y lookaround
+                        for (int i = Math.Max(0, x - 1); cnt < TooManyRolls && i <= Math.Min(x + 1, map[j].Length - 1); i++) // x lookaround
                             if (i == x && j == y)
                                 continue; // skip the roll I'm on.
-
-                            if (map[j][i] == Roll && ++cnt >= TooManyRolls)
-                                ok = false;
-                        }
-                    }
-
-                    if (ok)
+                            else if (map[j][i] == Roll) 
+                                ++cnt;
+                    if (cnt < TooManyRolls)
                     {
                         total++;
                         map[y][x] = changeFoundTo;
                     }
                 }
-            }
             return total;
         }
     }
